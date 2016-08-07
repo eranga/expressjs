c75eafdaaffdc750bc2e05e9766ae0486e04db41
Ubuntu <iranga3@t1ubuntu.ltfzipvbbl5e3m4mx3p0unp3na.fx.internal.cloudapp.net>
2016-08-07 20:13:06 +0000

    change 3

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('<h1>Hello Olympics! </h1>');
+  res.send('<h1>Hello Beautiful World! </h1>');
 });
 
 app.listen(3000, function () {
