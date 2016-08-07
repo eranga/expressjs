776f49eb6b77d8320da6f7bea511c3846ba41bca
Ubuntu <iranga3@t1ubuntu.ltfzipvbbl5e3m4mx3p0unp3na.fx.internal.cloudapp.net>
2016-08-07 19:51:13 +0000

    changed output message

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('<h1>Hello World! </h1>');
+  res.send('<h1>Hello Planet Earth! </h1>');
 });
 
 app.listen(3000, function () {
